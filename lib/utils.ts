2427d741eb965e27edf9f6ff0795d37ba167d913
wetoon <phiraphantanan@gmail.com>
2024-03-28 18:44:37 +0700

    update

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import crypto from "crypto";
 
 function _Encode( message : string ) {
-    const hmac = crypto.createHmac( 'sha3-256', "dd04f494146594a879fbb02ee011bc95887b8b5a01645fd23f1ceaf0b7a163c7" );
+    const hmac = crypto.createHmac( 'sha256', "dd04f494146594a879fbb02ee011bc95887b8b5a01645fd23f1ceaf0b7a163c7" );
     hmac.update( Buffer.from( String( message ), 'utf-8' ) );
     return hmac.digest("hex")
 }
